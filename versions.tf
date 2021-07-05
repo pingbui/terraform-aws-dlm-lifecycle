baeda0cfbd3c78bc5fa2b07bee04a421023dbf2c
Bui Ngoc Thanh <thanhbn87@gmail.com>
2021-07-05 17:52:02 +0700

    Fix duplicated required_providers ;))

--- versions.tf
+++ versions.tf
@@ -2,14 +2,6 @@ terraform {
   required_version = ">= 0.12.6"
 
   required_providers {
-    aws = ">= 3.35.0"
-  }
-}
-
-terraform {
-  required_version = ">= 0.12.6"
-
-  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.35.0"
